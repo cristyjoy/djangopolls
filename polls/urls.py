bd887604b7b54e22cd3beeab031b1c8a44431612
joyang <cristangengs.04@gmail.com>
2018-04-16 20:27:39 +0800

    Update poll detail template

--- polls/urls.py
+++ polls/urls.py
@@ -17,6 +17,9 @@ from django.contrib import admin
 from django.urls import path
 
 from . import views
+
+
+app_name = 'polls'
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
